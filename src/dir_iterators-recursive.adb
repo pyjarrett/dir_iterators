2e1fcc63ab6a31437096d352d5fe88f3702c1a77
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-09 22:55:27 -0400

    Remove Prepend_Vector since it's Ada 202x.

--- src/dir_iterators-recursive.adb
+++ src/dir_iterators-recursive.adb
@@ -1,14 +1,18 @@
-with Ada.Directories.Hierarchical_File_Names;
-
 package body Dir_Iterators.Recursive is
 
     package AD renames Ada.Directories;
-    package ADH renames Ada.Directories.Hierarchical_File_Names;
     package ASU renames Ada.Strings.Unbounded;
 
     ---------------------------------------------------------------------------
     -- Internals
     ---------------------------------------------------------------------------
+
+    -- Ada.Directories.Hierarchical_File_Names is optional, so manually provide
+    -- these.
+    function Is_Current_Directory_Name (Dir : String) return Boolean is
+       (Dir = ".");
+    function Is_Parent_Directory_Name (Dir : String) return Boolean is
+       (Dir = "..");
 
     -- Return true if the entry is "..".  The "." entry is used to find the
     -- starting directory and to report subdirectories immediately prior to
@@ -16,15 +20,16 @@ package body Dir_Iterators.Recursive is
     -- to detect it to prevent infinite recursion.  The actual entry of a
     -- directory needs to be skipped as well, because its "." version will
     -- cause it to be reported.
-    function Should_Skip (Dir_Entry : AD.Directory_Entry_Type) return Boolean is
+    function Should_Skip
+       (Dir_Entry : AD.Directory_Entry_Type) return Boolean is
         Name : constant String := AD.Simple_Name (Dir_Entry);
         use type AD.File_Kind;
     begin
         return
-            ADH.Is_Parent_Directory_Name (Name)
-            or else
-            (AD.Kind (Dir_Entry) = AD.Directory
-             and then not ADH.Is_Current_Directory_Name (Name));
+           Is_Parent_Directory_Name (Name)
+           or else
+           (AD.Kind (Dir_Entry) = AD.Directory
+            and then not Is_Current_Directory_Name (Name));
     end Should_Skip;
 
     -- Convenience override with a precondition.
@@ -38,15 +43,16 @@ package body Dir_Iterators.Recursive is
 
     --
     function Is_Current_Dir_Done
-        (It : Recursive_Dir_Iterator) return Boolean is
-        (not AD.More_Entries (It.Current_Search));
+       (It : Recursive_Dir_Iterator) return Boolean is
+       (not AD.More_Entries (It.Current_Search));
 
     -- Moves to the first entry which isn't the current or parent directory.
     -- Returns false if reaches the end of the current directory being
     -- iterated.
     function Next_In_Dir
-        (It : in out Recursive_Dir_Iterator) return Boolean with
-        Post => (if AD.More_Entries (It.Current_Search) then It.Has_Valid_Entry)
+       (It : in out Recursive_Dir_Iterator) return Boolean with
+        Post =>
+        (if AD.More_Entries (It.Current_Search) then It.Has_Valid_Entry)
     is
         use type AD.File_Kind;
     begin
@@ -55,13 +61,14 @@ package body Dir_Iterators.Recursive is
 
             It.Has_Valid_Entry := not Should_Skip (It.Next_Entry);
             if not It.Has_Valid_Entry
-                and then AD.Kind (It.Next_Entry) = AD.Directory
-                and then not ADH.Is_Parent_Directory_Name
-                    (AD.Simple_Name (It.Next_Entry))
-                and then not ADH.Is_Current_Directory_Name
-                    (AD.Simple_Name (It.Next_Entry)) then
+               and then AD.Kind (It.Next_Entry) = AD.Directory
+               and then not Is_Parent_Directory_Name
+                  (AD.Simple_Name (It.Next_Entry))
+               and then not Is_Current_Directory_Name
+                  (AD.Simple_Name (It.Next_Entry))
+            then
                 It.Current_Level.Append
-                    (ASU.To_Unbounded_String (AD.Full_Name (It.Next_Entry)));
+                   (ASU.To_Unbounded_String (AD.Full_Name (It.Next_Entry)));
             end if;
 
             if It.Has_Valid_Entry then
@@ -74,13 +81,13 @@ package body Dir_Iterators.Recursive is
     end Next_In_Dir;
 
     procedure Start_Search_In_Dir
-        (It : in out Recursive_Dir_Iterator; Dir : String) is
+       (It : in out Recursive_Dir_Iterator; Dir : String) is
         Filter : constant AD.Filter_Type :=
-            (AD.Ordinary_File | AD.Directory => True, others => False);
+           (AD.Ordinary_File | AD.Directory => True, others => False);
     begin
         AD.Start_Search
-            (Search => It.Current_Search, Directory => Dir, Pattern => "*",
-             Filter => Filter);
+           (Search => It.Current_Search, Directory => Dir, Pattern => "*",
+            Filter => Filter);
     end Start_Search_In_Dir;
 
     function Start (Dir : String) return Recursive_Dir_Iterator is
@@ -107,7 +114,7 @@ package body Dir_Iterators.Recursive is
 
         -- We're out of entries, so move onto the next depth.
         while Is_Current_Dir_Done (It) loop
-            It.Left_To_Process.Prepend_Vector (It.Current_Level);
+            It.Left_To_Process.Prepend (It.Current_Level);
             It.Current_Level.Clear;
 
             if It.Left_To_Process.Is_Empty then
@@ -116,7 +123,7 @@ package body Dir_Iterators.Recursive is
             end if;
 
             Start_Search_In_Dir
-                (It, ASU.To_String (It.Left_To_Process.First_Element));
+               (It, ASU.To_String (It.Left_To_Process.First_Element));
             It.Left_To_Process.Delete_First;
 
             if Next_In_Dir (It) then
@@ -128,15 +135,15 @@ package body Dir_Iterators.Recursive is
     function Done (It : Recursive_Dir_Iterator) return Boolean is
     begin
         return
-            not It.Has_Valid_Entry
-            and then not AD.More_Entries (It.Current_Search)
-            and then It.Left_To_Process.Is_Empty
-            and then It.Current_Level.Is_Empty;
+           not It.Has_Valid_Entry
+           and then not AD.More_Entries (It.Current_Search)
+           and then It.Left_To_Process.Is_Empty
+           and then It.Current_Level.Is_Empty;
     end Done;
 
     function Iterate
-        (Tree : Recursive_Dir_Walk)
-         return Recursive_Dir_Iterator_Interfaces.Forward_Iterator'Class is
+       (Tree : Recursive_Dir_Walk)
+        return Recursive_Dir_Iterator_Interfaces.Forward_Iterator'Class is
     begin
         return Start (ASU.To_String (Tree.Root));
     end Iterate;
@@ -154,20 +161,20 @@ package body Dir_Iterators.Recursive is
     end Has_Element;
 
     overriding function First
-        (Object : Recursive_Dir_Iterator) return Cursor is
+       (Object : Recursive_Dir_Iterator) return Cursor is
     begin
         return Cursor'(It => Object'Unrestricted_Access);
     end First;
 
     overriding function Next
-        (It : Recursive_Dir_Iterator; Position : Cursor) return Cursor is
+       (It : Recursive_Dir_Iterator; Position : Cursor) return Cursor is
     begin
         Next (Position.It.all);
         return Position;
     end Next;
 
     function Element_Value
-        (Tree : Recursive_Dir_Walk; Position : Cursor) return Reference_Type is
+       (Tree : Recursive_Dir_Walk; Position : Cursor) return Reference_Type is
     begin
         pragma Unreferenced (Tree);
         return Reference_Type'(Element => Position.It.Next_Entry'Access);
